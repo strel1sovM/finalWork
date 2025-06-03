0ad442e66eb37f82707366f215a2548c478eadc8
strel1sovM <t1409584@gmail.com>
2025-06-03 21:39:27 +0500

    netly

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,17 @@
-    import { dirname, resolve } from 'node:path'
-    import { fileURLToPath } from 'node:url'
-    import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
-    const __dirname = dirname(fileURLToPath(import.meta.url))
-
-    export default defineConfig({
-        esbuild: {
-            target: 'esnext'
-        },
-        build: {
-            rollupOptions: {
-                input: {
-                    main: resolve(__dirname, 'index.html'),
-                    home: resolve(__dirname, 'src/pages/pojects.html'),
-                    signup: resolve(__dirname, 'src/pages/signup.html'),
-                    404: resolve(__dirname, 'src/pages/404.html'),
-                    account: resolve(__dirname, 'src/pages/account.html'),
-                },
-            },
-        },
-    })
+export default defineConfig({
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        home: resolve(__dirname, 'src/pages/projects.html'),
+        signup: resolve(__dirname, 'src/pages/signup.html'),
+        '404': resolve(__dirname, 'src/pages/404.html'),
+        account: resolve(__dirname, 'src/pages/account.html'),
+      }
+    }
+  }
+});
