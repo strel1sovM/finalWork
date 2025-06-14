eafeb7c29bc9b001c8c5356d9489897e47793b8c
strel1sovM <t1409584@gmail.com>
2025-06-14 12:26:44 +0500

    new in dist

--- client/src/scripts/setup.js
+++ client/src/scripts/setup.js
@@ -1,63 +1,63 @@
 
-import '../assets/signin.css';
-console.log(`main.logged`);
+  import '../assets/signin.css';
+  console.log(`main.logged`);
 
 
-import { cookie } from '../utils/cookie.js';
-import { useToast } from '../utils/hooks.js';
-import axios from 'axios';
+  import { cookie } from '../utils/cookie.js';
+  import { useToast } from '../utils/hooks.js';
+  import axios from 'axios';
 
-const overlay = document.querySelector('#loading-overlay');
-
-
-const app = document.getElementById('app');
-console.log(app);
-
-const form = document.querySelector('#my-form-signin');
+  const overlay = document.querySelector('#loading-overlay');
 
 
-export function loadALL(params) {
+  const app = document.getElementById('app');
+  console.log(app);
 
-  if (!form) {
-    return
-  }
-  console.log(`loadAll activated`);
+  const form = document.querySelector('#my-form-signin');
 
-  form.onsubmit = async (e) => {
-    e.preventDefault();
 
-    overlay.classList.remove('hidden');
+  export function loadALL(params) {
 
-    const user = {};
-    const fm = new FormData(form);
-    fm.forEach((value, key) => (user[key] = value));
-
-    try {
-      const response = await axios.post(import.meta.env.VITE_API_URL + '/authentication', {
-        ...user,
-        strategy: 'local'
-      });
-      
-
-      cookie.setCookie('accessToken', response.data.accessToken, 1);
-      useToast('success', 'Вы успешно вошли!');
-      setTimeout(() => {
-        window.location.href = '/home';
-      }, 1000);
-    } catch (error) {
-      useToast('error', 'Ошибка входа: ' + (error.code || 'Неизвестная'));
-    } finally {
-      overlay.classList.add('hidden');
+    if (!form) {
+      return
     }
-  };
+    console.log(`loadAll activated`);
+
+    form.onsubmit = async (e) => {
+      e.preventDefault();
+
+      overlay.classList.remove('hidden');
+
+      const user = {};
+      const fm = new FormData(form);
+      fm.forEach((value, key) => (user[key] = value));
+
+      try {
+        const response = await axios.post(import.meta.env.VITE_API_URL + '/authentication', {
+          ...user,
+          strategy: 'local'
+        });
+        
+
+        cookie.setCookie('accessToken', response.data.accessToken, 1);
+        useToast('success', 'Вы успешно вошли!');
+        setTimeout(() => {
+          window.location.href = '/home';
+        }, 1000);
+      } catch (error) {
+        useToast('error', 'Ошибка входа: ' + (error.code || 'Неизвестная'));
+      } finally {
+        overlay.classList.add('hidden');
+      }
+    };
 
 
-}
-try {
-  loadALL()
-} finally {
+  }
+  try {
+    loadALL()
+  } finally {
 
-}
+  }
 
 
 
